0898d992f68e02655ffbac1dc70ac4f542c9b006
annisadiann <annisadian0412@gmail.com>
2025-12-25 16:34:35 +0700

    memperbaiki RepositoriDataSiswa.kt

--- app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
@@ -3,20 +3,19 @@ package com.example.praktikum8.repositori
 import com.example.praktikum8.apiservice.ServiceApiSiswa
 import com.example.praktikum8.modeldata.DataSiswa
 
-interface RepositoryDataSiswa {
+interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    suspend fun SatuSiswa(id: Int) : DataSiswa
-    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
-    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-): RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa
+    private  val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa
         .getSiswa()
-
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 
