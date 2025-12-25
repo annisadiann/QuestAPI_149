470f1f2377d163ea735becf2ef2964bfebccd600
annisadiann <annisadian0412@gmail.com>
2025-12-25 16:09:42 +0700

    Menambahkan initializer EditViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
@@ -25,6 +25,11 @@ object PenyediaViewModel {
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
-
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
