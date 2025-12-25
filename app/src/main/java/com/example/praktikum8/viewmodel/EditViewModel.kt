dda32bd3b84048cf05b9a60260032f5b9c43b249
annisadiann <annisadian0412@gmail.com>
2025-12-25 15:54:24 +0700

    Menambahkan EditViewModel dan inisialisasi UI state siswa

--- app/src/main/java/com/example/praktikum8/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/EditViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.praktikum8.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum8.modeldata.DetailSiswa
+import com.example.praktikum8.modeldata.UIStateSiswa
+import com.example.praktikum8.modeldata.toDataSiswa
+import com.example.praktikum8.modeldata.toUiStateSiswa
+import com.example.praktikum8.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+
+
