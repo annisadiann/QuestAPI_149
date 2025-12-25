44647988be61e74352d3696337281aba40321bb0
annisadiann <annisadian0412@gmail.com>
2025-12-25 15:56:24 +0700

    Menambahkan fungsi updateUiState untuk memperbarui state dan validasi input siswa

--- app/src/main/java/com/example/praktikum8/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/EditViewModel.kt
@@ -28,6 +28,12 @@ RepositoryDataSiswa
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+
 
 
 
