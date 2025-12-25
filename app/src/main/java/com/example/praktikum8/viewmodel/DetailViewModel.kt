e6b14012d4d641a2d1c3693ecf680ed92f7c5495
annisadiann <annisadian0412@gmail.com>
2025-12-25 16:00:16 +0700

    Menambahkan StatusUIDetail untuk menangani kondisi loading, sukses, dan error

--- app/src/main/java/com/example/praktikum8/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/DetailViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.praktikum8.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum8.modeldata.DataSiswa
+import com.example.praktikum8.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+import retrofit2.Response
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+
